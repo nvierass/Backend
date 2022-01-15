0f6f55ffe337f909fa80f2a2ccb0c2edbbc4e146
Nicolas <nicolas.vieras@usach.cl>
2022-01-14 21:27:14 -0300

    Main

---
+++ src/main/java/Mingeso/Evaluacion2/Evaluacion2Application.java
@@ -0,0 +1,11 @@
+package Mingeso.Evaluacion2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Evaluacion2Application {
+    public static void main(String[] args){
+        SpringApplication.run(Evaluacion2Application.class,args);
+    }
+}
